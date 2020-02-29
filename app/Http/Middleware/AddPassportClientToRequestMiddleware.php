d0e5a31c538dabbdfacd39da5c45b634d31d1a17
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-02-29 15:59:22 +0200

    Add password client

--- app/Http/Middleware/AddPassportClientToRequestMiddleware.php
+++ app/Http/Middleware/AddPassportClientToRequestMiddleware.php
@@ -4,9 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Passport\Repositories\ClientRepository;
 
 class AddPassportClientToRequestMiddleware
 {
+    /**
+     * @var ClientRepository
+     */
+    protected $clientRepository;
+
+    public function __construct(ClientRepository $clientRepository)
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -17,6 +28,16 @@ class AddPassportClientToRequestMiddleware
     public function handle($request, Closure $next)
     {
         if ($this->isTokenRoute($request) && $request->input('grant_type') === 'password') {
+            $passwordClient = $this->clientRepository->getPasswordClient();
+
+            if (!$passwordClient) {
+                return $next($request);
+            }
+
+            $request->merge([
+                'client_id' => $passwordClient->id,
+                'client_secret' => $passwordClient->secret,
+            ]);
         }
 
         return $next($request);
