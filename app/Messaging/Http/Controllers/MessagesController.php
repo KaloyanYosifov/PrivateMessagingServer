4427bba320bcc01a8a7e0b37c6bc6fe193ff8d66
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-03-07 21:26:45 +0200

    Order by not latest

--- app/Messaging/Http/Controllers/MessagesController.php
+++ app/Messaging/Http/Controllers/MessagesController.php
@@ -14,7 +14,7 @@ class MessagesController
 {
     public function index(Request $request)
     {
-        $query = Message::whereNotNull('id');
+        $query = Message::whereNotNull('id')->orderBy('created_at', 'ASC');
 
         if ($conversationId = $request->input('conversation_id')) {
             $query->where('conversation_id', $conversationId);
