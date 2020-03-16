f346f938e32bd4c1054215a0ef2cff48e3e8d01e
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-03-16 14:51:09 +0200

    Add validation to check if we already have an email

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email,NULL',
             'password' => 'required|confirmed|min:6',
         ];
     }
